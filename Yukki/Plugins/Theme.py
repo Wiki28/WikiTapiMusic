3578ea98fb6b32aa8f347c9339a91758c997a4ca
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-22 06:55:12 +0800

    Update Theme.py

--- Yukki/Plugins/Theme.py
+++ Yukki/Plugins/Theme.py
@@ -34,10 +34,10 @@ __HELP__ = """
 
 
 /settheme
-- Set a theme for thumbnails.
+- Tetapkan tema untuk gambar mini.
 
 /theme
-- Check Theme for your chat.
+- Periksa Tema untuk obrolan Anda.
 """
 
 
